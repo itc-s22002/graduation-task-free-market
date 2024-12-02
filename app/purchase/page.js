62f508ec7080d5136c5840c1ab9897ed00070859
s22002 <s22002>
2024-12-02 16:14:01 +0900

    vrcelエラー解決

--- app/purchase/page.js
+++ app/purchase/page.js
@@ -20,7 +20,6 @@ import { useRouter } from "next/navigation";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { Suspense } from "react";
 
-
 const db = getFirestore(app);
 const auth = getAuth(app);
 
@@ -118,7 +117,7 @@ const Purchase = () => {
         body: JSON.stringify({
           subject: "test",
           text: tex,
-          email:user.email
+          email: user.email,
         }),
       });
 
@@ -159,8 +158,7 @@ const Purchase = () => {
     router.push("/");
   };
 
-
-  const NegotiationsSuspended = (u) =>{
+  const NegotiationsSuspended = (u) => {
     const currentUrl = window.location.href;
     handleSendEmail(`交渉を取り消しました ${currentUrl}`);
     try {
@@ -182,7 +180,7 @@ const Purchase = () => {
       alert("アップロードに失敗しました。");
     }
     router.push("/");
-  }
+  };
 
   const NegotiationButton = (i, u) => {
     if (item.statas == "交渉中" && item.seller_id != user.uid) {
@@ -217,11 +215,8 @@ const Purchase = () => {
     }
   };
 
-  
-
   return (
-    <Suspense fallback={<div>Loding...</div>}>
-        <div className={styles.box}>
+    <div className={styles.box}>
       <Header />
       <div className={styles.container}>
         {item && (
@@ -259,9 +254,14 @@ const Purchase = () => {
         )}
       </div>
     </div>
-
-    </Suspense>
   );
 };
 
-export default Purchase;
+const Page = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Purchase />
+    </Suspense>
+  );
+};
+export default Page;
